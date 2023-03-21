17a6e4407672ef22d37217f676bd3fdfb94175b6
rakaxirpl19 <rka.nm135@gmail.com>
2023-03-21 22:28:36 +0700

    first commit

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <div className="text-center h-32 flex items-center justify-center">
-      <p className="text-xl text-gray-800">&copy; Copyright 2022 <span className="font-bold">Canddev</span></p>
+      <p className="text-xl text-gray-800">&copy; BY <span className="font-bold">RAKA</span></p>
     </div>
   );
 };
